09b3672485a2ab32563f6ef5201ca3b258088d07
niwei_notebook <yodoo_admin@163.com>
2019-09-24 13:15:28 +0800

    test commnet

---
+++ rootinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p /opt/help   && cd /opt/help
+git clone --depth 1 https://github.com/niwei2008/fzf.git
+git clone --depth 1 https://github.com/niwei2008/navi.git
+/opt/help/fzf/install --key-bindings      --completion      --update-rc   &&      source ~/.bashrc
+cd /opt/help/navi/ && sudo make install
