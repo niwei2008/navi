06210035a1a35a04eb8eefeb3baf83068bdd77f3
niwei_notebook <yodoo_admin@163.com>
2019-09-24 15:21:59 +0800

    add ubuntu

---
+++ ubuntuinstall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p /opt/help   && cd /opt/help
+git clone --depth 1 https://github.com/niwei2008/fzf.git
+git clone --depth 1 https://github.com/niwei2008/navi.git
+/opt/help/fzf/install --key-bindings      --completion      --update-rc   &&      source ~/.bashrc
+cd /opt/help/navi/ && sudo make install
+ln -s /usr/local/bin/navi /bin/adm
+
+apt install npm -y
+npm install -g tldr
+tldr --update
+chmod 777 /tmp/tldr/
+
+echo "in case of"
+echo "ln -s /opt/nodejs/lib/node_modules/tldr/bin/tldr /bin"
